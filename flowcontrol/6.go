a129ee0b6584933cd4863626a682bf0dc277474e
uuutee <uuuteenet@gmail.com>
2018-06-21 01:13:31 +0900

    Update

---
+++ flowcontrol/6.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"math"
+	"fmt"
+)
+
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	}
+	return lim
+}
+
+func main() {
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20),
+	)
+}
+
+// 9 20
---
+++ flowcontrol/7.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"math"
+	"fmt"
+)
+
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	} else {
+		fmt.Printf("%g >= %g\n", v, lim)
+	}
+	// can't use v here, though
+	return lim
+}
+
+func main() {
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20),
+	)
+}
+
+// 27 >= 20
+// 9 20
